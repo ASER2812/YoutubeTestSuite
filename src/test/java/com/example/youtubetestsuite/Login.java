237b26c9cd0588847c3f35bac47fc8b092c67e4e
ASER2812 <101837939+ASER2812@users.noreply.github.com>
2024-04-16 11:34:26 -0400

    initial

---
+++ src/test/java/com/example/youtubetestsuite/Login.java
@@ -0,0 +1,20 @@
+package com.example.youtubetestsuite;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.*;
+import java.time.Duration;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import static org.testng.Assert.*;
+
+public class Login {
+}
